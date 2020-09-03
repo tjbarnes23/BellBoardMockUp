99496b7e6c56004282ef7bdcca27df3f52c243bc
Tim Barnes <tjbarnes23@gmail.com>
2020-09-03 18:21:50 -0400

    Initial performance entry controls

---
+++ BellBoardMockUp/Models/Performance.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BellBoardMockUp.Models
+{
+    public class Performance
+    {
+        public Style Style { get; set; }
+
+        public string StyleOther { get; set; }
+
+        public BellsPerRinger BellsPerRinger { get; set; }
+
+        public string NumBells { get; set; }
+
+
+    }
+}
---
+++ BellBoardMockUp/Pages/Index.razor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BellBoardMockUp.Models;
+
+namespace BellBoardMockUp.Pages
+{
+    public partial class Index
+    {
+        public Index()
+        {
+            Performance = new Performance();
+        }
+
+        public Performance Performance { get; set; }
+
+        public void StyleChanged(Style value)
+        {
+            Performance.Style = value;
+        }
+
+        public void BellsPerRingerChanged(BellsPerRinger value)
+        {
+            Performance.BellsPerRinger = value;
+        }
+
+        public void NumBellsChanged(string value)
+        {
+            Performance.NumBells = value;
+        }
+    }
+}
