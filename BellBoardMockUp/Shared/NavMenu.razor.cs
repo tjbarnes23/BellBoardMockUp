160dfcb2a5054a25a01c87673e49a80b065e3304
Tim Barnes <tjbarnes23@gmail.com>
2020-09-30 17:41:13 -0400

    Implemented collapsible menus for small screens

---
+++ BellBoardMockUp/Shared/NavMenu.razor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BellBoardMockUp.Shared
+{
+    public partial class NavMenu
+    {
+        private bool collapseNavMenu = true;
+
+        private string NavMenuCssClass => collapseNavMenu ? "collapse" : null;
+
+        /*
+        private string NavMenuCssClass()
+        {
+            if (collapseNavMenu == true)
+            {
+                return "collapse";
+            }
+            else
+            {
+                return null;
+            }
+        }
+        */
+
+        private void ToggleNavMenu()
+        {
+            collapseNavMenu = !collapseNavMenu;
+        }
+    }
+}
